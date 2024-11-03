7fd75b7d8fcd3366e3779d428d38dfb672ca2b41
system64fumo <system64fumo@tuta.io>
2024-11-03 23:45:42 +0200

    Support syshud's latest changes

--- src/libs/syshud.cpp
+++ src/libs/syshud.cpp
@@ -1,4 +1,5 @@
 #include "../main.hpp"
+#include <filesystem>
 
 void load_libsyshud() {
 	void* handle = dlopen("libsyshud.so", RTLD_LAZY);
@@ -18,58 +19,41 @@ void load_libsyshud() {
 
 	std::cout << "Loading: libsyshud.so" << std::endl;
 
-	config_hud cfg;
-	config_parser config(std::string(getenv("HOME")) + "/.config/sys64/hud/config.conf");
+	std::string config_path;
+	std::map<std::string, std::map<std::string, std::string>> config;
+	std::map<std::string, std::map<std::string, std::string>> config_usr;
 
-	std::string cfg_position = config.get_value("main", "position");
-	if (!cfg_position.empty())
-		cfg.position = cfg_position;
+	bool cfg_sys = std::filesystem::exists("/usr/share/sys64/hud/config.conf");
+	bool cfg_sys_local = std::filesystem::exists("/usr/local/share/sys64/hud/config.conf");
+	bool cfg_usr = std::filesystem::exists(std::string(getenv("HOME")) + "/.config/sys64/hud/config.conf");
 
-	std::string cfg_orientation = config.get_value("main", "orientation");
-	if (!cfg_orientation.empty())
-		cfg.orientation = cfg_orientation[0];
+	// Load default config
+	if (cfg_sys)
+		config_path = "/usr/share/sys64/hud/config.conf";
+	else if (cfg_sys_local)
+		config_path = "/usr/local/share/sys64/hud/config.conf";
+	else
+		std::fprintf(stderr, "No default config found, Things will get funky!\n");
 
-	std::string cfg_width = config.get_value("main", "width");
-	if (!cfg_width.empty())
-		cfg.width = std::stoi(cfg_width);
+	config = config_parser(config_path).data;
 
-	std::string cfg_height = config.get_value("main", "height");
-	if (!cfg_height.empty())
-		cfg.height = std::stoi(cfg_height);
+	// Load user config
+	if (cfg_usr)
+		config_path = std::string(getenv("HOME")) + "/.config/sys64/hud/config.conf";
+	else
+		std::fprintf(stderr, "No user config found\n");
 
-	std::string cfg_icon_size = config.get_value("main", "icon_size");
-	if (!cfg_icon_size.empty())
-		cfg.icon_size = std::stoi(cfg_icon_size);
+	config_usr = config_parser(config_path).data;
 
-	std::string cfg_percentage = config.get_value("main", "percentage");
-	if (cfg_percentage == "true")
-		cfg.show_percentage = true;
-	else if (cfg_percentage == "false")
-		cfg.show_percentage = false;
+	// Merge configs
+	for (const auto& [key, nested_map] : config_usr)
+		config[key] = nested_map;
 
-	std::string cfg_margins = config.get_value("main", "margins");
-	if (!cfg_margins.empty())
-		cfg.margins = cfg_margins;
+	// Sanity check
+	if (!(cfg_sys || cfg_sys_local || cfg_usr)) {
+		std::fprintf(stderr, "No config available, Something ain't right here.");
+		return;
+	}
 
-	std::string cfg_timeout = config.get_value("main", "timeout");
-	if (!cfg_timeout.empty())
-		cfg.desired_timeout = std::stoi(cfg_timeout);
-
-	std::string cfg_transition = config.get_value("main", "transition");
-	if (!cfg_transition.empty())
-		cfg.transition_time = std::stoi(cfg_transition);
-
-	std::string cfg_backlight = config.get_value("main", "backlight");
-	if (cfg_backlight != "empty")
-		cfg.backlight_path = cfg_backlight;
-
-	std::string cfg_monitors = config.get_value("main", "monitors");
-	if (!cfg_monitors.empty())
-		cfg.monitors = cfg_monitors;
-
-	std::string cfg_keyboard = config.get_value("main", "keyboard");
-	if (cfg_keyboard != "empty")
-		cfg.keyboard = cfg_keyboard;
-
-	syshud_window = syshud_create(cfg);
+	syshud_window = syshud_create(config);
 }
