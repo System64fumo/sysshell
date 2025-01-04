4e564461eea490e3099e1a6418eb2b333362d92b
system64fumo <system64fumo@tuta.io>
2025-01-04 18:04:44 +0200

    Fix syspower crash
    Update the config to match upstream syspower's config.
    (Ideally i should update the config system so this never happens but i never got around to it so for now this will do)

--- include/libsyspower.hpp
+++ include/libsyspower.hpp
@@ -1,9 +1,18 @@
 #pragma once
+#include <unordered_map>
 
 struct config_power {
 	int position = 4;
 	int main_monitor = 0;
 	int transition_duration = 1000;
+	std::unordered_map<uint, std::string> hotkeys = {
+		{0x075, "shutdown"},
+		{0x072, "reboot"},
+		{0x06c, "logout"},
+		{0x073, "suspend"},
+		{0x063, "cancel"},
+		{0xff1b, "cancel"}
+	};
 };
 
 class syspower {};
